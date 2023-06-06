ffd2a5963da709e1cdaf8588cd205e3135e9c05b
dianprnms <purnamasaridian010@gmail.com>
2023-06-06 15:13:26 +0700

    First Commit

---
+++ app/src/main/java/com/aerotech/flytix/view/Checkout.kt
@@ -0,0 +1,20 @@
+package com.aerotech.flytix.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.aerotech.flytix.R
+
+class Checkout : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_checkout, container, false)
+    }
+
+}
