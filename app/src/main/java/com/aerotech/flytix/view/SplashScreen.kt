b315469e72bb44cc5f1205ac5b44c85bc4ee77b0
heru prayogo <heruprayogo062@gmail.com>
2023-06-06 21:08:25 +0700

    First Commit

--- app/src/main/java/com/aerotech/flytix/view/SplashScreen.kt
+++ app/src/main/java/com/aerotech/flytix/view/SplashScreen.kt
@@ -2,11 +2,12 @@ package com.aerotech.flytix.view
 
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.aerotech.flytix.R
 
@@ -14,9 +15,6 @@ import com.aerotech.flytix.R
 @Suppress("DEPRECATION")
 class SplashScreen : Fragment() {
     private lateinit var progressBar: ProgressBar
-    private val progressIncrement = 20 // Increment percentage for each update
-    private val progressInterval = 500 // Interval in milliseconds between progress updates
-    private val totalProgress = 100 // Total progress percentage
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,30 +30,12 @@ class SplashScreen : Fragment() {
         simulateLoading()
     }
 
-//    private fun simulateProgress() {
-//        val handler = Handler(Looper.getMainLooper())
-//        var progress = 0
-//
-//        handler.postDelayed(object : Runnable {
-//            override fun run() {
-//                progress += progressIncrement
-//                progressBar.progress = progress
-//
-//                if (progress < totalProgress) {
-//                    handler.postDelayed(this, progressInterval.toLong())
-//                } else {
-//                    findNavController().navigate(R.id.action_splashScreen_to_home2)
-//                }
-//            }
-//        }, progressInterval.toLong())
-//    }
-
     private fun simulateLoading() {
         progressBar = requireView().findViewById(R.id.progressBar)
         val handler = Handler()
         val progressRunnable = Runnable {
             var progress = 0
-            while (progress <= 1000) {
+            while (progress <= 100) {
                 progressBar.progress = progress
                 progress += 5
                 handler.postDelayed({ }, 300)
