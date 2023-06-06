a9d609caccb89ecbe5b29ce7314055b06d942daf
LordKajoc <rizkinuriman@upi.edu>
2023-06-06 19:48:31 +0700

    Done Bottom Navigation

--- app/src/main/java/com/aerotech/flytix/MainActivity.kt
+++ app/src/main/java/com/aerotech/flytix/MainActivity.kt
@@ -2,10 +2,43 @@ package com.aerotech.flytix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.aerotech.flytix.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomnav)
+        //navcontroler,nacdestination,bundle
+        navController.addOnDestinationChangedListener { navcontroler, destination, bundle ->
+
+            when (destination.id) {
+                R.id.detail -> {
+                    bottomNavView.visibility = View.GONE
+                }
+                else -> {
+                    bottomNavView.visibility = View.VISIBLE
+                }
+            }
+        }
+        NavigationUI.setupWithNavController(bottomNavView, navController)
+    }
+
+    fun clearBackStackInclusive(tag: String?) {
+        supportFragmentManager.popBackStack(tag, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
 }
