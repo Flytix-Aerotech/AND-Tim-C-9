6830588f95f1b027c99cbc9a85d7563a4fbd27f7
LordKajoc <rizkinuriman@upi.edu>
2023-06-06 14:18:58 +0700

    first commit

---
+++ app/src/main/java/com/aerotech/flytix/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aerotech.flytix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
