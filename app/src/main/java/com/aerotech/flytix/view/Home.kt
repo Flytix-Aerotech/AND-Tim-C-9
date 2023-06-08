7bd588def8fbb12bbfcf556611a679271167e3b1
dianprnms <purnamasaridian010@gmail.com>
2023-06-09 03:53:54 +0700

    data diri, login, register, lupa password

--- app/src/main/java/com/aerotech/flytix/view/Home.kt
+++ app/src/main/java/com/aerotech/flytix/view/Home.kt
@@ -1,32 +1,53 @@
 package com.aerotech.flytix.view
 
+import android.content.Context
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.aerotech.flytix.R
 import com.aerotech.flytix.databinding.FragmentHomeBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 
 class Home : Fragment() {
 
-    private lateinit var binding: FragmentHomeBinding
+    private lateinit var bottomSheetDialog: BottomSheetDialog
+    lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        binding.btnCaripenerbangan.setOnClickListener{
-//            val fragmentB = Login()
-//            fragmentB.show(parentFragmentManager,fragmentB.tag)
-//        }
+        binding.btnCariPenerbangan.setOnClickListener {
+            findNavController().navigate(R.id.action_home2_to_login2)
+        }
     }
 
+    private fun showBottomSheet() {
+        val bottomSheetView = layoutInflater.inflate(R.layout.bottom_sheet, null)
+        bottomSheetDialog = BottomSheetDialog(context?.applicationContext!!)
+        bottomSheetDialog.setContentView(bottomSheetView)
+
+        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetView.parent as View)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+        val bottomSheetButton = bottomSheetView.findViewById<Button>(R.id.bottomSheetButton)
+        bottomSheetButton.setOnClickListener {
+            bottomSheetDialog.dismiss()
+        }
+
+        bottomSheetDialog.show()
+    }
 }
