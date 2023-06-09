145353ed6ef83b7e2ce2ae647d7815ea785935c3
heru prayogo <heruprayogo062@gmail.com>
2023-06-09 21:34:52 +0700

    update Result

--- app/src/main/java/com/aerotech/flytix/view/ResultSearch.kt
+++ app/src/main/java/com/aerotech/flytix/view/ResultSearch.kt
@@ -5,18 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.aerotech.flytix.R
+import com.aerotech.flytix.databinding.FragmentResultSearchBinding
+
 
 class ResultSearch : Fragment() {
-
+    lateinit var binding : FragmentResultSearchBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result_search, container, false)
+        binding = FragmentResultSearchBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
+
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        adapter = HariAdapter(ArrayList())
+//        binding.rvHari.layoutManager = LinearLayoutManager(context,  LinearLayoutManager.HORIZONTAL, false)
+//        binding.rvHari.adapter = adapter
+//        setVM = ViewModelProvider(this).get(SetVM::class.java)
+//        setVM.getData()
+//        setVM.getDataHari.observe(viewLifecycleOwner, Observer {
+//            adapter.setItemDataFilm(it as ArrayList<DataHari>)
+//        })
+//    }
 
 
 }
