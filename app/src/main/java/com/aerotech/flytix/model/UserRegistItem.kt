0038900a2746b3c200f374d3a77f6f40f473def5
heru prayogo <heruprayogo062@gmail.com>
2023-06-12 19:04:34 +0700

    update Register

---
+++ app/src/main/java/com/aerotech/flytix/model/UserRegistItem.kt
@@ -0,0 +1,18 @@
+package com.aerotech.flytix.model
+
+import com.google.gson.annotations.SerializedName
+
+data class UserRegistItem (
+    @SerializedName("email")
+    val email: String,
+    @SerializedName("full_name")
+    val fullName: String,
+    @SerializedName("password")
+    val password: String,
+    @SerializedName("phone_number")
+    val phoneNumber: String,
+    @SerializedName("photo")
+    val photo: String,
+    @SerializedName("username")
+    val username: String
+    )
