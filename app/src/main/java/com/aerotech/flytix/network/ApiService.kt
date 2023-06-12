40d7d52702a32e084d03932339b267548e7d24d5
dianprnms <purnamasaridian010@gmail.com>
2023-06-12 18:13:02 +0700

    login

--- app/src/main/java/com/aerotech/flytix/network/ApiService.kt
+++ app/src/main/java/com/aerotech/flytix/network/ApiService.kt
@@ -1,9 +1,11 @@
 package com.aerotech.flytix.network
 
+import com.aerotech.flytix.model.NewUser
+import retrofit2.Call
 import retrofit2.http.POST
 
 interface ApiService {
 
-    @POST("auth/register")
-    fun postRegister()
+    @POST("auth/login")
+    fun postLogin(): Call<List<NewUser>>
 }
