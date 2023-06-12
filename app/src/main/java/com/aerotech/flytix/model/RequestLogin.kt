e299a7cbf86e6b348213cbf0f8038ee877a85719
dianprnms <purnamasaridian010@gmail.com>
2023-06-13 02:29:15 +0700

    Login (Finish)

---
+++ app/src/main/java/com/aerotech/flytix/model/RequestLogin.kt
@@ -0,0 +1,10 @@
+package com.aerotech.flytix.model
+
+import com.google.gson.annotations.SerializedName
+
+data class RequestLogin (
+    @SerializedName("email")
+    val email: String,
+    @SerializedName("password")
+    val password: String
+    )
