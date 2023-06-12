30af4d3ccff0483e4bab41fde4dd90fca0a7e2b0
dianprnms <purnamasaridian010@gmail.com>
2023-06-12 16:54:34 +0700

    Senin, 12 Juni

--- app/src/main/java/com/aerotech/flytix/network/ApiService.kt
+++ app/src/main/java/com/aerotech/flytix/network/ApiService.kt
@@ -1,11 +1,9 @@
 package com.aerotech.flytix.network
 
-import com.aerotech.flytix.model.NewUser
-import retrofit2.Call
 import retrofit2.http.POST
 
 interface ApiService {
 
-    @POST("auth/login")
-    fun postLogin(): Call<List<NewUser>>
+    @POST("auth/register")
+    fun postRegister()
 }
--- app/src/main/java/com/aerotech/flytix/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/aerotech/flytix/viewmodel/UserViewModel.kt
@@ -1,35 +1,14 @@
 package com.aerotech.flytix.viewmodel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.aerotech.flytix.model.NewUser
 import com.aerotech.flytix.network.ApiService
-import dagger.hilt.android.lifecycle.HiltViewModel
-import retrofit2.Call
 import retrofit2.Callback
-import retrofit2.Response
 import javax.inject.Inject
-@HiltViewModel
 
 class UserViewModel @Inject constructor(private val Client: ApiService): ViewModel() {
     private var livedataUser: MutableLiveData<List<NewUser>> = MutableLiveData()
-    val dataLoginUser: LiveData<List<NewUser>> get() = livedataUser
 
-    fun UserLogin(){
-        Client.postLogin().enqueue(object :Callback<List<NewUser>> {
-            override fun onResponse(call: Call<List<NewUser>>, response: Response<List<NewUser>>) {
-                if (response.isSuccessful){
-                    livedataUser.postValue(response.body())
-                }else{
-                    livedataUser.postValue(emptyList())
-                }
-            }
 
-            override fun onFailure(call: Call<List<NewUser>>, t: Throwable) {
-                livedataUser.postValue(emptyList())
-            }
-
-        })
-    }
 }
