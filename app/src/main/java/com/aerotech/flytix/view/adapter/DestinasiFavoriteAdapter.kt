ad6d7149bfc8a7e5f91469a81327425149b8fae6
dianprnms <purnamasaridian010@gmail.com>
2023-06-29 00:02:38 +0700

    Destinasi favorite ke result search

--- app/src/main/java/com/aerotech/flytix/view/adapter/DestinasiFavoriteAdapter.kt
+++ app/src/main/java/com/aerotech/flytix/view/adapter/DestinasiFavoriteAdapter.kt
@@ -1,8 +1,12 @@
 package com.aerotech.flytix.view.adapter
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.collection.arrayMapOf
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import com.aerotech.flytix.R
 import com.aerotech.flytix.databinding.ItemDestinasiFavoriteBinding
 import com.aerotech.flytix.model.ticket.destinasifavorite.Data
 
@@ -20,10 +24,47 @@ class DestinasiFavoriteAdapter(private var listDataDestinasi: List<Data>): Recyc
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var bulan = getTanggal(listDataDestinasi[position].flight.arrivalDate.subSequence(5,7).toString())
+
         holder.binding.tvAsal.text = listDataDestinasi[position].flight.departureLocation
         holder.binding.tvTujuan.text = listDataDestinasi[position].flight.arrivalLocation
         holder.binding.tvPesawat.text = listDataDestinasi[position].flight.airline
+        holder.binding.tvTanggal.text = listDataDestinasi[position].flight.departureDate.subSequence(7,10).toString() + " - " +
+                                        listDataDestinasi[position].flight.arrivalDate.subSequence(7,10).toString() + bulan +
+                                        listDataDestinasi[position].flight.arrivalDate.subSequence(0, 4).toString()
         holder.binding.tvHarga.text = listDataDestinasi[position].price.toString()
 
+        holder.itemView.setOnClickListener { view ->
+            val bundle = Bundle().apply {
+                putString("TanggalKeberangkatan", listDataDestinasi[position].flight.departureDate)
+                putString("TanggalKembali", listDataDestinasi[position].flight.arrivalDate)
+                putString("KotaKeberangkatan", listDataDestinasi[position].flight.departureLocation)
+                putString("KotaDestinasi", listDataDestinasi[position].flight.arrivalLocation)
+                putString("KelasKursi", listDataDestinasi[position].typeOfClass)
+                //putString("price", listDataDestinasi[position].price)
+            }
+
+            val navController = Navigation.findNavController(view)
+            navController.navigate(R.id.action_home3_to_resultSearch, bundle)
+        }
+
+    }
+
+    fun getTanggal(bulan: String): String? {
+        val arr = arrayMapOf<String, String>()
+        arr["01"] = " Januari "
+        arr["02"] = " Februari "
+        arr["03"] = " Maret "
+        arr["04"] = " April "
+        arr["05"] = " Mei "
+        arr["06"] = " Juni "
+        arr["07"] = " Juli "
+        arr["08"] = " Agustus "
+        arr["09"] = " September "
+        arr["10"] = " Oktober "
+        arr["11"] = " November "
+        arr["12"] = " Desember "
+
+        return arr[bulan]
     }
 }
--- app/src/main/java/com/aerotech/flytix/view/home/Home.kt
+++ app/src/main/java/com/aerotech/flytix/view/home/Home.kt
@@ -272,6 +272,14 @@ class Home : Fragment(),
                     }
                 }
             })
+//        binding.rvHome.setOnClickListener {
+//            findNavController().navigate(R.id.action_home3_to_resultSearch, bund)
+//        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        getDestinasiFavorite()
     }
 }
 
